7a5b9bf9e503dafde5c465bd83613e0e2bf02311
GameDevDenis <denisbrunodionisio@gmail.com>
2026-05-06 18:25:47 -0300

    1.1 Commit
    -Arrumado o problema das funções.
    -Sistema de laço no menu de gerenciar a conta.

--- contabancaria.py
+++ contabancaria.py
@@ -8,26 +8,28 @@ class Conta:
         self.endereco = endereco
         self.saldo = saldo
 
+    def deposito_saldo(self):
+        valor = float(input ("Digite o valor do deposito: "))
+        if valor > 0.00:
+            self.saldo += valor
+            print (f" O valor de {valor} foi adicionado, agora você tem: {self.saldo}.\n\n\n")
+        else:
+            print("Valor inválido")
 
-'''def deposito_saldo (self, valor):
-    valor = float(input ("Digite o valor do deposito: "))
-    if valor > 0.00:
-        self.saldo+=valor
-        print (f" O valor de {self.valor} foi adicionado, agora você tem: {self.saldo}.")'''
+    def mostrar_saldo(self):
+        print (f"Seu saldo atual é: {self.saldo}.\n\n\n")
 
-'''def mostrar_saldo (self):
-    print (f"Seu saldo atual é: {self.saldo}.")'''
-
-'''def sacar_saldo (self, valor):
+    def sacar_saldo(self):
         valor = float(input("Digite o valor que deseja sacar: "))
         if valor > self.saldo:
-            print(f"O valor do saque não pode ser superior ao do saldo.")
+            print(f"O valor do saque não pode ser superior ao do saldo.\n\n\n")
         elif valor <= 0.00:
-            print(f"O valor de saque não pode ser zero ou inferior a zero.")
+            print(f"O valor de saque não pode ser zero ou inferior a zero.\n\n\n")
         else:
-            print ("O valor do saque foi efetuado.")'''
+            self.saldo -= valor
+            print ("O valor do saque foi efetuado.\n\n\n")
 
-    
+
 contas ={  
     0: Conta (0,"Elliot","000.000.000-00","Avenida Brasil",200.00),
     1: Conta (1,"Shane","001.001.001-01","Rua dos Pericanos",50.00),
@@ -37,34 +39,30 @@ contas ={
 while (True):
     print("BEM-VINDO AO BANCO:")
     conta = int(input("Digite a conta de acesso: "))
+    
     if conta in contas:
-        print(f"Bem vindo {conta}\n Oque deseja fazer?")
-        print("1 = depositar: \n2 = mostrar o saldo \n3 = sacar")
+        usuario = contas[conta]
+        while (True):
+            print(f"Bem vindo {usuario.nome}\n Oque deseja fazer?")
+            print("1 = depositar: \n2 = mostrar o saldo \n3 = sacar \n4 = sair")
         
-        escolha = int(input("digite o número: "))
-        if escolha == 1:
-            valor = float(input ("Digite o valor do deposito: "))
-            if valor > 0.00:
-                saldo+=valor
-                print (f" O valor de {valor} foi adicionado, agora você tem: {conta.saldo}.")
-            else:
-                print("Valor inválido")
+        
+            escolha = int(input("digite o número: "))
 
-        elif escolha == 2:
-            print (f"Seu saldo atual é: {conta.saldo}.")
+            if escolha == 1:
+                usuario.deposito_saldo()
 
-        elif escolha == 3:
-            valor = float(input("Digite o valor que deseja sacar: "))
-            if valor > conta.saldo:
-                print(f"O valor do saque não pode ser superior ao do saldo.")
-            elif valor <= 0.00:
-                print(f"O valor de saque não pode ser zero ou inferior a zero.")
+            elif escolha == 2:
+                usuario.mostrar_saldo()
+
+            elif escolha == 3:
+                usuario.sacar_saldo()
+
+            elif escolha == 4:
+                break
+
             else:
-                print ("O valor do saque foi efetuado.")
-        else:
-            print("\n\n\nOpção inválida\n\n\n")
+                print("\n\n\nOpção inválida\n\n\n")
 
     else:
         print("\n\n\nConta Invalida.\n\n\n")
-        
-
